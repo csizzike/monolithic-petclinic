e4cf59ac9ae2943e1dfca7562067bcde0ff89706
Kokoska Martin <martin.kokoska@ilogics.com>
2023-01-12 14:12:27 +0100

    URL to vets moved to app.properties

--- monolith/src/main/java/org/springframework/samples/petclinic/vet/service/VetServiceClient.java
+++ monolith/src/main/java/org/springframework/samples/petclinic/vet/service/VetServiceClient.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.vet.service;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.samples.petclinic.vet.dto.VetDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,11 +11,14 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class VetServiceClient implements HasVets {
 
+    @Value("${spring.vet.microservice.url}")
+    private String vetsUrl;
+
     @Override
     public Collection<VetDTO> allVetDTOs() {
         RestTemplate restTemplate = new RestTemplate();
 
-        VetDTO[] empArray = restTemplate.getForObject("http://localhost:8081/vets",
+        VetDTO[] empArray = restTemplate.getForObject(vetsUrl,
                 VetDTO[].class);
         return Arrays.asList(empArray);
     }
